a0260abe56f78cd50c9a2d384318a814bf39108c
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-02 20:31:33 +0300

    Initial project structure

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <HeaderComponent />
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
+<style scoped>
 #nav a.router-link-exact-active {
   color: #42b983;
 }
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
